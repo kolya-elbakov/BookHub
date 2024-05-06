cdf80330e7171842ddcc8dd4108e52fc4261b2b1
kolya-elbakov <mr.elbakov@mail.ru>
2024-05-06 18:46:32 +0300

    добавление LoginTest

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -43,41 +43,23 @@ class MessageController extends Controller
         })->count();
 
         if (!$recipient->is_profile_open && $messageCount === 0) {
-            $message = new Message();
-            $message->sender_id = auth()->user()->id;
-            $message->recipient_id = $id;
-            $message->message = $validated['message'];
-            $message->save();
 
-            if ($message) {
-                $this->rabbitMQService->publish('notification_queue', $message->id);
-            }
-        } elseif (!$recipient->is_profile_open && $messageCount >= 1) {
-            if ($messageCount === 1) {
+            } elseif (!$recipient->is_profile_open && $messageCount === 1) {
                 return back()->withErrors(['message' => 'Профиль закрыт. Вы уже отправили приветственное сообщение.']);
-            } elseif(!$recipient->is_profile_open && $messageCount >= 2) {
-                $message = new Message();
-                $message->sender_id = auth()->user()->id;
-                $message->recipient_id = $id;
-                $message->message = $validated['message'];
-                $message->save();
+            } elseif (!$recipient->is_profile_open && $messageCount >= 2) {
 
-                if ($message) {
-                    $this->rabbitMQService->publish('notification_queue', $message->id);
-                }
-            }
-        } else {
-            $message = new Message();
-            $message->sender_id = auth()->user()->id;
-            $message->recipient_id = $id;
-            $message->message = $validated['message'];
-            $message->save();
+            } else {
 
-            if ($message) {
-                $this->rabbitMQService->publish('notification_queue', $message->id);
             }
+
+        $message = new Message();
+        $message->sender_id = auth()->user()->id;
+        $message->recipient_id = $id;
+        $message->message = $validated['message'];
+        $message->save();
+        if ($message) {
+            $this->rabbitMQService->publish('notification_queue', $message->id);
         }
-
         return redirect()->back();
     }
 
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_login_returns_a_successful_response(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $this->withoutMiddleware();
+        $user = User::create([
+            'name' => 'kolya',
+            'surname' => 'elbakov',
+            'email' => 'test@mail.ru',
+            'password' => Hash::make('12345678')
+        ]);
+        $response =  $this->withSession(['_token' => csrf_token()])->post('/login', [
+            'email' => $user->email,
+            'password' => '12345678'
+        ]);
+
+        $response->assertRedirect('/books');
+        $this->assertAuthenticatedAs($user);
+    }
+}
